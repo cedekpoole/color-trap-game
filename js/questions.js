7fe33bc3e4c69bc94636333174f5ca232536de1b
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-06 13:54:26 +0100

    create example question + add script to html doc

---
+++ js/questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    question: "blue",
+    color: "#FEE12B",
+    options: {
+      one: {
+        word: "Blue",
+        wordColor: "red",
+      },
+      two: {
+        word: "Yellow",
+        wordColor: "green"
+      },
+      three: {
+        word: "Blue",
+        wordColor: "orange"
+      },
+      four: {
+        word: "Purple",
+        wordColor: "#FEE12B"
+      }
+    },
+  },
+];
