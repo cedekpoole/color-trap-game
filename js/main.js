ad21d80251a000450fc12c0ac521cf64be91ef06
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-07 14:03:47 +0100

    create working countdown

--- js/main.js
+++ js/main.js
@@ -9,9 +9,28 @@ const timer = document.querySelector(".timer");
 const questionTitle = document.querySelector("#question-title");
 const choicesEl = document.querySelector("#choices");
 const feedbackEl = document.querySelector('#feedback')
+const timeEl = document.querySelector("#time")
+
+
 let questionNumber = 0;
 let q = questions[questionNumber];
 let timeInSeconds = 30;
+
+// create countdown for when game starts 
+const countdown = () => {
+    let timeInterval = setInterval(function () {
+      timeInSeconds--;
+      timeEl.textContent = timeInSeconds;
+      if (timeInSeconds <= 0 || questionNumber === questions.length) {
+        clearInterval(timeInterval);
+        // If the user reaches 0 seconds, it is the end of the quiz (they score a 0)
+        if (timeLeft < 0) {
+          timeLeft = 0;
+        }
+        // EndQuiz();
+      }
+    }, 1000);
+  };
 
 // Create event listener for when start button is clicked
 startButton.addEventListener("click", () => {
@@ -23,6 +42,7 @@ startButton.addEventListener("click", () => {
 
   questionNumber = 0;
   renderQuestion();
+  countdown();
 });
 
 const renderQuestion = () => {
@@ -47,12 +67,12 @@ choicesEl.addEventListener("click", (e) => {
     } else {
       feedbackEl.textContent = "Wrong Answer!";
       feedbackEl.style.color = "#89023E";
-      timeLeft -= 5;
+      timeInSeconds -= 5;
     }
   }
   setTimeout(function () {
     feedbackEl.style.display = "none";
-  }, 750);
+  }, 500);
 
   questionNumber++;
   q = questions[questionNumber]
