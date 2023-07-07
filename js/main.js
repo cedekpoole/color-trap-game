5a1dccf01f4b96b5f0594f48f3d379c7c0f523ae
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-07 13:06:56 +0100

    hide highscores button + show time left and add image to question screen

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ const startScreen = document.querySelector("#start-screen")
 
 // Question screen elements
 const questionWrapper = document.querySelector("#questions")
+const timer = document.querySelector(".timer")
+const scores = document.querySelector(".scores")
+
 
 startButton.addEventListener("click", () => {
     // hide start screen and show question screen when start button clicked
     startScreen.setAttribute("class", "hide");
-    questionWrapper.removeAttribute("class")
+    questionWrapper.removeAttribute("class", "hide")
+    timer.removeAttribute("class", "hide")
+    scores.setAttribute("class", "hide")
 })
