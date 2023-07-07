91e0fc72562c6de19270d634bfb730a409176b24
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-07 15:59:55 +0100

    add event listener to submit button

---
+++ js/scores.js
@@ -0,0 +1,25 @@
+const initials = document.querySelector("#initials");
+const submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (initials.value === "") {
+    alert("Please enter your initials!");
+  } else {
+    // check if there has been previous high scores in local storage
+    const previousScores =
+      JSON.parse(window.localStorage.getItem("highscore")) || [];
+      // create object for user details 
+      const userDetails = {
+        score: timeInSeconds,
+        name: initials.value
+      }
+
+      // add new user to local storage
+      previousScores.push(userDetails)
+      window.localStorage.setItem("highscore", JSON.stringify(previousScores))
+
+      // when button is pressed, move user to highscores html page
+      window.location.href = "highscores.html"
+  }
+});
