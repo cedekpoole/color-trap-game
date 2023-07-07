aec193245a8aa24ef3e2c630bf8972f4a070337c
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-07 13:25:37 +0100

    render first question correctly

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
 // Start screen elements
-const startButton = document.querySelector("#start")
-const startScreen = document.querySelector("#start-screen")
+const startButton = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
+const scores = document.querySelector(".scores");
 
 // Question screen elements
-const questionWrapper = document.querySelector("#questions")
-const timer = document.querySelector(".timer")
-const scores = document.querySelector(".scores")
+const questionWrapper = document.querySelector("#questions");
+const timer = document.querySelector(".timer");
+const questionTitle = document.querySelector("#question-title");
+const choicesEl = document.querySelector("#choices")
 
+let questionNumber = 0;
+let q = questions[questionNumber];
 
+// Create event listener for when start button is clicked
 startButton.addEventListener("click", () => {
     // hide start screen and show question screen when start button clicked
     startScreen.setAttribute("class", "hide");
-    questionWrapper.removeAttribute("class", "hide")
-    timer.removeAttribute("class", "hide")
-    scores.setAttribute("class", "hide")
+    questionWrapper.removeAttribute("class", "hide");
+    timer.removeAttribute("class", "hide");
+    scores.setAttribute("class", "hide");
+
+    questionNumber = 0;
+    renderQuestion();
 })
+
+const renderQuestion = () => {
+    questionTitle.textContent = q.question
+    questionTitle.style.color = q.color
+
+    let answerOptions = "";
+    for (const option in q.options) {
+        answerOptions += `<button>${q.options[option]}</button>`
+    }
+    choicesEl.innerHTML = answerOptions;
+}
+
