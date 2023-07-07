c327d2ab955e515af9577b4143bf3823fb334c34
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-07-07 12:13:32 +0100

    create set of questions

--- js/questions.js
+++ js/questions.js
@@ -1,24 +1,132 @@
 const questions = [
   {
-    question: "blue",
+    question: "Blue",
     color: "#FEE12B",
-    options: {
-      one: {
-        word: "Blue",
-        wordColor: "red",
-      },
-      two: {
-        word: "Yellow",
-        wordColor: "green"
-      },
-      three: {
-        word: "Blue",
-        wordColor: "orange"
-      },
-      four: {
-        word: "Purple",
-        wordColor: "#FEE12B"
-      }
-    },
+    options: [
+        "Green",
+        "Red",
+        "Black",
+        "Blue",
+        "Yellow",
+        "Orange"
+
+    ]
   },
+  {
+    question: "Red",
+    color: "orange",
+    options: [
+        "Orange",
+        "Blue",
+        "Black",
+        "Red",
+        "Green",
+        "Yellow"
+
+    ]
+  },
+  {
+    question: "Orange",
+    color: "black",
+    options: [
+        "Green",
+        "Orange",
+        "Blue",
+        "Black",
+        "Yellow",
+        "Red"
+
+    ]
+  },
+  {
+    question: "Green",
+    color: "yellow",
+    options: [
+        "Black",
+        "Yellow",
+        "Green",
+        "Red",
+        "Blue",
+        "Orange"
+
+    ]
+  },
+  {
+    question: "Yellow",
+    color: "red",
+    options: [
+        "Red",
+        "Green",
+        "Blue",
+        "Black",
+        "Orange",
+        "Yellow"
+
+    ]
+  },
+  {
+    question: "Blue",
+    color: "black",
+    options: [
+        "Orange",
+        "Yellow",
+        "Black",
+        "Blue",
+        "Red",
+        "Green"
+
+    ]
+  },
+  {
+    question: "Red",
+    color: "green",
+    options: [
+        "Yellow",
+        "Green",
+        "Orange",
+        "Red",
+        "Blue",
+        "Black"
+
+    ]
+  },
+  {
+    question: "Blue",
+    color: "yellow",
+    options: [
+        "Yellow",
+        "Blue",
+        "Black",
+        "Red",
+        "Green",
+        "Orange"
+
+    ]
+  },
+  {
+    question: "Orange",
+    color: "blue",
+    options: [
+        "Blue",
+        "Orange",
+        "Green",
+        "Black",
+        "Yellow",
+        "Red"
+
+    ]
+  },
+  {
+    question: "Black",
+    color: "green",
+    options: [
+        "Red",
+        "Black",
+        "Green",
+        "Blue",
+        "Yellow",
+        "Orange"
+
+    ]
+  }
 ];
